35db03709e1b2ecd8446e55e12c691055dce64c6
jrporto2 <86452124+jrporto2@users.noreply.github.com>
2026-04-06 16:24:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+set -x
+#curl -s https://raw.githubusercontent.com/jrporto2/odoo-18-docker-compose/refs/heads/master/run.sh | sudo bash -s odoo-one 10017 20017 password
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-
+MASTERPASSWORD=${4:-adminpasswd}
+#clear directory
+rm -rf $DESTINATION
 # Clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-18-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/jrporto2/odoo-17-docker-compose.git $DESTINATION
 rm -rf $DESTINATION/.git
-
-# Create PostgreSQL directory
-mkdir -p $DESTINATION/postgresql
-
+mkdir -p $DESTINATION/datadrive/odoo/{addons,etc,filestore,logs}
+mkdir -p $DESTINATION/datadrive/nginx/{certs,conf.d,logs}
+mkdir -p $DESTINATION/datadrive/postgres/{db,custom-init-scripts}
+mkdir -p $DESTINATION/datadrive/pgadmin/data
 # Change ownership to current user and set restrictive permissions for security
 sudo chown -R $USER:$USER $DESTINATION
 sudo chmod -R 700 $DESTINATION  # Only the user has access
-
+sudo scp /etc/ssl/certs/origin_certificate.pem $DESTINATION/datadrive/nginx/certs/origin_certificate.pem
+sudo scp /etc/ssl/certs/origin_private_key.pem $DESTINATION/datadrive/nginx/certs/origin_private_key.pem
+sudo chmod +x $DESTINATION/entrypoint.sh
 # Check if running on macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Running on macOS. Skipping inotify configuration."
@@ -26,31 +32,29 @@ else
   fi
   sudo sysctl -p
 fi
-
-# Set ports in docker-compose.yml
+# Set ports in .env file
 # Update docker-compose configuration
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS sed syntax
-  sed -i '' 's/10018/'$PORT'/g' $DESTINATION/docker-compose.yml
-  sed -i '' 's/20018/'$CHAT'/g' $DESTINATION/docker-compose.yml
+  sed -i '' 's/10017/'$PORT'/g' $DESTINATION/.env
+  sed -i '' 's/20017/'$CHAT'/g' $DESTINATION/.env
 else
   # Linux sed syntax
-  sed -i 's/10018/'$PORT'/g' $DESTINATION/docker-compose.yml
-  sed -i 's/20018/'$CHAT'/g' $DESTINATION/docker-compose.yml
+  sed -i 's/10017/'$PORT'/g' $DESTINATION/.env
+  sed -i 's/20017/'$CHAT'/g' $DESTINATION/.env
+  sed -i 's/adminpasswd/'$MASTERPASSWORD'/g' $DESTINATION/datadrive/odoo/etc/odoo.conf 
 fi
 
 # Set file and directory permissions after installation
 find $DESTINATION -type f -exec chmod 644 {} \;
 find $DESTINATION -type d -exec chmod 755 {} \;
-
-chmod +x $DESTINATION/entrypoint.sh
-
+echo $DESTINATION
+sudo chown -R 999:999 $DESTINATION/datadrive/postgres
+sudo chown -R 999:999 $DESTINATION/datadrive/postgres/db
+sudo chown -R 101:101 $DESTINATION/datadrive/odoo
+sudo chown -R 5050:5050 $DESTINATION/datadrive/pgadmin
+sudo chown -R 101:101 $DESTINATION/datadrive/nginx
 # Run Odoo
-if ! is_present="$(type -p "docker-compose")" || [[ -z $is_present ]]; then
-  docker compose -f $DESTINATION/docker-compose.yml up -d
-else
-  docker-compose -f $DESTINATION/docker-compose.yml up -d
-fi
+docker compose -f $DESTINATION/docker-compose.yml up -d
 
-
-echo "Odoo started at http://localhost:$PORT | Master Password: minhng.info | Live chat port: $CHAT"
+echo "Odoo started at http://localhost:$PORT | Master Password: $MASTERPASSWORD | Live chat port: $CHAT"
