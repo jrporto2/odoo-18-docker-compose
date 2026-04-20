232b5655eda552c7247fd4a762b33950355a8ed9
jrporto2 <86452124+jrporto2@users.noreply.github.com>
2026-04-20 15:35:02 -0500

    Change ownership and permissions for Odoo directories
    Update permissions for Odoo sessions and filestore directories.

--- run.sh
+++ run.sh
@@ -18,8 +18,8 @@ mkdir -p $DESTINATION/datadrive/pgadmin/data
 #sudo chown -R $USER:$USER $DESTINATION
 sudo chown -R 101:101 $DESTINATION
 sudo chmod -R 700 $DESTINATION  # Only the user has access
-sudo chmod -R 755 ~/odoo-one/datadrive/odoo/sessions
-sudo chmod -R 755 ~/odoo-one/datadrive/odoo/filestore
+sudo chmod -R 755 $DESTINATION/datadrive/odoo/sessions
+sudo chmod -R 755 $DESTINATION/datadrive/odoo/filestore
 sudo scp /etc/ssl/certs/origin_certificate.pem $DESTINATION/datadrive/nginx/certs/origin_certificate.pem
 sudo scp /etc/ssl/certs/origin_private_key.pem $DESTINATION/datadrive/nginx/certs/origin_private_key.pem
 sudo chmod +x $DESTINATION/entrypoint.sh
